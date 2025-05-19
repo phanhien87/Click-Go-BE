9af33618fefdf0d18e589f6e1d59374c41a5d62d
ADMIN-PC\Admin <phanminhhien782004@gmail.com>
2025-05-19 21:29:28 +0700

    Done

---
+++ DTOs/GetPostDto.cs
@@ -0,0 +1,11 @@
+﻿using Click_Go.Models;
+
+namespace Click_Go.DTOs
+{
+    public class GetPostDto
+    {
+        public PostReadDto Post { get; set; }
+        public List<GetCommentByPostDto> Comment { get; set; }
+        public OverallCriteriaDto Rating { get; set; }
+    }
+}
--- Services/Interfaces/IPostService.cs
+++ Services/Interfaces/IPostService.cs
@@ -8,7 +8,7 @@ namespace Click_Go.Services.Interfaces
     public interface IPostService
     {
         Task<Post> CreatePostAsync(PostCreateDto postDto, string userId);
-        Task<Post> GetPostByIdAsync(long id);
+        Task<GetPostDto> GetPostByIdAsync(long id);
         Task<IEnumerable<Post>> GetPostsByUserIdAsync(string userId);
     }
 } 

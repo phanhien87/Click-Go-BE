ff65d9d7f0c83642c145ff9452da67ede73f3e95
Long Doan <longdhhe186717@fpt.edu.vn>
2025-05-31 08:59:54 +0700

    Update on CreatePost and SearchPost

--- Services/Interfaces/IPostService.cs
+++ Services/Interfaces/IPostService.cs
@@ -10,7 +10,7 @@ namespace Click_Go.Services.Interfaces
         Task<Post> CreatePostAsync(PostCreateDto postDto, string userId);
         Task<GetPostDto> GetPostByIdAsync(long id);
         Task<IEnumerable<GetPostDto>> GetPostsByUserIdAsync(string userId);
-        Task<IEnumerable<PostReadDto>> SearchByAddressAsync(string addressQuery);
+        Task<IEnumerable<PostReadDto>> SearchPostsAsync(PostSearchDto searchDto);
         Task<IEnumerable<PostReadDto>> GetAllPostsAsync();
     }
 }

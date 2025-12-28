c4741fb06fb1a6394e102343fe305bf817a72b98
Phan Hiển <phanminhhien782004@gmail.com>
2025-12-28 22:35:27 +0700

    review

--- Kafka/KafkaTopicCreator.cs
+++ Kafka/KafkaTopicCreator.cs
@@ -9,7 +9,7 @@ namespace Click_Go.Kafka
         {
             var config = new AdminClientConfig
             {
-                BootstrapServers = "localhost:9092" // sửa theo config của bạn
+                BootstrapServers = "localhost:9092"
             };
 
             using (var adminClient = new AdminClientBuilder(config).Build())
@@ -17,8 +17,8 @@ namespace Click_Go.Kafka
                 try
                 {
                     string topicName = "search-queries";
-                    int numPartitions = 3; // số partition tùy bạn
-                    short replicationFactor = 1; // nếu chỉ có 1 broker thì để 1
+                    int numPartitions = 3; 
+                    short replicationFactor = 1; 
 
                     var topicSpec = new TopicSpecification
                     {
@@ -28,16 +28,16 @@ namespace Click_Go.Kafka
                     };
 
                     await adminClient.CreateTopicsAsync(new List<TopicSpecification> { topicSpec });
-                    Console.WriteLine($"✅ Topic '{topicName}' created successfully.");
+                    Console.WriteLine($"Topic '{topicName}' created successfully.");
                 }
                 catch (CreateTopicsException e)
                 {
                     foreach (var result in e.Results)
                     {
                         if (result.Error.Code != ErrorCode.TopicAlreadyExists)
-                            Console.WriteLine($"❌ An error occurred: {result.Error.Reason}");
+                            Console.WriteLine($"An error occurred: {result.Error.Reason}");
                         else
-                            Console.WriteLine($"⚠️ Topic '{result.Topic}' already exists.");
+                            Console.WriteLine($"Topic '{result.Topic}' already exists.");
                     }
                 }
             }
